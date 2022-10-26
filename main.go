bc04dfc80ac648f54c1dbcf34470c88b16d7f7d8
Farrid Kuntoro <farridkun6@outlook.com>
2022-10-26 18:57:18 +0700

    setup: integrate MongoDB into project

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/farridkun/go-echo-challenge/configs"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hey it works")
+	resp := echo.New()
+
+	configs.ConnectDB()
+
+	resp.Logger.Fatal(resp.Start(":9999"))
 }
